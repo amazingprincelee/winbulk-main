cb205acf72402b3f8e0685bd4118ff04ff5e9226
D'Amazing Prince Lee <prince_lee.nna85@ymail.com>
2023-10-09 14:57:40 +0100

    remove header picture on mobile view

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
                                 </div>
 
                             </div>
-                            <button className='hidden lg:flex justify-end text-xl font-semibold py-4 px-6 lg:px-12 navbutton text-white'>Get Started</button>
+                            <button className='hidden lg:flex justify-end text-xl font-semibold py-4 px-6 lg:px-12 navbutton text-white'><a href="https://t.me/OfficialWinbulk" target='_blank'>Telegram</a></button>
                             {/* <Contactusform /> */}
                         </div>
 
